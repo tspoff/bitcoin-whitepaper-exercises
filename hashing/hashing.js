47726a3dff1ccbfa752affd14a06c7da983e5ca4
tspoff <tspoff@gmail.com>
2018-05-02 11:27:03 -0400

    Hashing section implementation

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -19,19 +19,22 @@ var Blockchain = {
 	blocks: [],
 };
 
-// Genesis block
-Blockchain.blocks.push({
+const GENESIS_BLOCK = {
 	index: 0,
 	hash: "000000",
 	data: "",
 	timestamp: Date.now(),
-});
+};
+
+// Genesis block
+Blockchain.blocks.push(GENESIS_BLOCK);
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line);
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
@@ -39,5 +42,63 @@ Blockchain.blocks.push({
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		`${bl.prevHash};${bl.index};${JSON.stringify(bl.data)};${bl.timestamp}`
 	).digest("hex");
+}
+
+function createBlock(data) {
+	let block = {
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		index: Blockchain.blocks.length,
+		data: data,
+		timestamp: Date.now(),
+	}
+	block.hash = blockHash(block);
+
+	if (verifyBlock(block)) {
+		Blockchain.blocks.push(block);
+		return block;
+	}
+	else {
+		return false;
+	}
+}
+
+function verifyChain() {
+	if (Blockchain.blocks.length === 0) {
+		return false;
+	}
+
+	//Ensure genesis block has genesis properties:
+	if (Blockchain.blocks[0].hash != GENESIS_BLOCK.hash ||
+		Blockchain.blocks[0].index != GENESIS_BLOCK.index) {
+		return false;
+	}
+
+	for (let i = 1; i < Blockchain.blocks.length; i++) {
+
+		const prevBlock = Blockchain.blocks[i - 1];
+		const currentBlock = Blockchain.blocks[i];
+
+		if (typeof currentBlock.data != "string" ||
+			currentBlock.index != i ||
+			currentBlock.hash != blockHash(currentBlock) ||
+			currentBlock.prevHash != prevBlock.hash ||
+			currentBlock.timestamp < prevBlock.timestamp) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function verifyBlock(block) {
+	if (block.hash === blockHash(block) &&
+		block.data != null &&
+		typeof block.data === 'string'	
+	) {
+		return true;
+	} else {
+		return false;
+	}
 }
