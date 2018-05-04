7b6d7fec7f0ffd68999ef8368f1390c1663311e8
tspoff <tspoff@gmail.com>
2018-05-04 11:17:19 -0400

    Wallet js started

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -3,28 +3,26 @@
 var path = require("path");
 var fs = require("fs");
 
-var Blockchain = require(path.join(__dirname,"blockchain.js"));
+var Blockchain = require(path.join(__dirname, "blockchain.js"));
 
-const KEYS_DIR = path.join(__dirname,"keys");
-const PRIV_KEY_TEXT_1 = fs.readFileSync(path.join(KEYS_DIR,"1.priv.pgp.key"),"utf8");
-const PUB_KEY_TEXT_1 = fs.readFileSync(path.join(KEYS_DIR,"1.pub.pgp.key"),"utf8");
-const PRIV_KEY_TEXT_2 = fs.readFileSync(path.join(KEYS_DIR,"2.priv.pgp.key"),"utf8");
-const PUB_KEY_TEXT_2 = fs.readFileSync(path.join(KEYS_DIR,"2.pub.pgp.key"),"utf8");
+const KEYS_DIR = path.join(__dirname, "keys");
+const PRIV_KEY_TEXT_1 = fs.readFileSync(path.join(KEYS_DIR, "1.priv.pgp.key"), "utf8");
+const PUB_KEY_TEXT_1 = fs.readFileSync(path.join(KEYS_DIR, "1.pub.pgp.key"), "utf8");
+const PRIV_KEY_TEXT_2 = fs.readFileSync(path.join(KEYS_DIR, "2.priv.pgp.key"), "utf8");
+const PUB_KEY_TEXT_2 = fs.readFileSync(path.join(KEYS_DIR, "2.pub.pgp.key"), "utf8");
 
 var wallet = {
 	accounts: {},
 };
 
-addAccount(PRIV_KEY_TEXT_1,PUB_KEY_TEXT_1);
-addAccount(PRIV_KEY_TEXT_2,PUB_KEY_TEXT_2);
+addAccount(PRIV_KEY_TEXT_1, PUB_KEY_TEXT_1);
+addAccount(PRIV_KEY_TEXT_2, PUB_KEY_TEXT_2);
 
 // fake an initial balance in account #1
-wallet.accounts[PUB_KEY_TEXT_1].outputs.push(
-	{
-		account: PUB_KEY_TEXT_1,
-		amount: 42,
-	}
-);
+wallet.accounts[PUB_KEY_TEXT_1].outputs.push({
+	account: PUB_KEY_TEXT_1,
+	amount: 42,
+});
 
 main().catch(console.log);
 
@@ -33,31 +31,42 @@ main().catch(console.log);
 
 async function main() {
 	await spend(
-		/*from=*/wallet.accounts[PUB_KEY_TEXT_1],
-		/*to=*/wallet.accounts[PUB_KEY_TEXT_2],
-		/*amount=*/13
+		/*from=*/
+		wallet.accounts[PUB_KEY_TEXT_1],
+		/*to=*/
+		wallet.accounts[PUB_KEY_TEXT_2],
+		/*amount=*/
+		13
 	);
 
 	await spend(
-		/*from=*/wallet.accounts[PUB_KEY_TEXT_2],
-		/*to=*/wallet.accounts[PUB_KEY_TEXT_1],
-		/*amount=*/5
+		/*from=*/
+		wallet.accounts[PUB_KEY_TEXT_2],
+		/*to=*/
+		wallet.accounts[PUB_KEY_TEXT_1],
+		/*amount=*/
+		5
 	);
 
 	await spend(
-		/*from=*/wallet.accounts[PUB_KEY_TEXT_1],
-		/*to=*/wallet.accounts[PUB_KEY_TEXT_2],
-		/*amount=*/31
+		/*from=*/
+		wallet.accounts[PUB_KEY_TEXT_1],
+		/*to=*/
+		wallet.accounts[PUB_KEY_TEXT_2],
+		/*amount=*/
+		31
 	);
 
 	try {
 		await spend(
-			/*from=*/wallet.accounts[PUB_KEY_TEXT_2],
-			/*to=*/wallet.accounts[PUB_KEY_TEXT_1],
-			/*amount=*/40
+			/*from=*/
+			wallet.accounts[PUB_KEY_TEXT_2],
+			/*to=*/
+			wallet.accounts[PUB_KEY_TEXT_1],
+			/*amount=*/
+			40
 		);
-	}
-	catch (err) {
+	} catch (err) {
 		console.log(err);
 	}
 
@@ -66,7 +75,7 @@ async function main() {
 	console.log(await Blockchain.verifyChain(Blockchain.chain));
 }
 
-function addAccount(privKey,pubKey) {
+function addAccount(privKey, pubKey) {
 	wallet.accounts[pubKey] = {
 		privKey,
 		pubKey,
@@ -74,10 +83,55 @@ function addAccount(privKey,pubKey) {
 	};
 }
 
-async function spend(fromAccount,toAccount,amountToSpend) {
+async function spend(fromAccount, toAccount, amountToSpend) {
 	// TODO
+	var trData = {
+		inputs: [],
+		outputs: [],
+	};
+
+	// pick inputs to use from fromAccount's outputs (i.e. previous txns, see line 22), sorted descending
+	var sortedInputs = fromAccounts.outputs.sort((a, b) => b - a);
+
+	let inputTotal = 0;
+		for (let input of sortedInputs) {
+			// remove input from output-list
+
+			// do we have enough inputs to cover the spent amount?
+			if (inputTotal >= amountToSpend) {
+
+			}
+		}
+
+		if (inputAmounts < amountToSpend) {
+
+			throw `Don't have enough to spend ${amountToSpend}!`;
+		}	
+
+	// sign and record inputs
+
+	// record output
+
+	// is "change" output needed?
+
+	// create transaction and add it to blockchain
+	var tr = Blockchain.createTransaction(trData);
+	Blockchain.insertBlock(
+		// TODO .createBlock method
+
+	);
+
+	// record outputs in our wallet (if needed)	
 }
 
 function accountBalance(account) {
-	// TODO
+	var balance = 0;
+
+	// 	if (account in wallet.accounts) {
+
+	// 	}	
+
+
+	// 	return balance;
+
 }
