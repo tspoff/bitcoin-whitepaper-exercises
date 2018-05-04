2cecf06c37e0ed8819017b17052ba4099ed98ddf
tspoff <tspoff@gmail.com>
2018-05-04 13:55:15 -0400

    pow implementation

--- pow/pow.js
+++ pow/pow.js
@@ -2,6 +2,31 @@
 
 var crypto = require("crypto");
 
+const hexToBinaryLookup = {
+	'0': '0000',
+	'1': '0001',
+	'2': '0010',
+	'3': '0011',
+	'4': '0100',
+	'5': '0101',
+	'6': '0110',
+	'7': '0111',
+	'8': '1000',
+	'9': '1001',
+	'a': '1010',
+	'b': '1011',
+	'c': '1100',
+	'd': '1101',
+	'e': '1110',
+	'f': '1111',
+	'A': '1010',
+	'B': '1011',
+	'C': '1100',
+	'D': '1101',
+	'E': '1110',
+	'F': '1111'
+};
+
 // The Power of a Smile
 // by Tupac Shakur
 var poem = [
@@ -37,7 +62,6 @@ for (let line of poem) {
 	difficulty++;
 }
 
-
 // **********************************
 
 function createBlock(data) {
@@ -46,19 +70,59 @@ function createBlock(data) {
 		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
 		data,
 		timestamp: Date.now(),
+		nonce: 0
 	};
 
 	bl.hash = blockHash(bl);
 
+	let proofOfWorkFound = false;
+	while (!proofOfWorkFound) {
+	
+		if (hashIsLowEnough(bl.hash)) {
+			proofOfWorkFound = true;
+		}
+
+		else {
+			bl.nonce += 1;
+			bl.hash = blockHash(bl);
+		}
+		
+	}
 	return bl;
 }
 
 function blockHash(bl) {
 	// TODO
+	return crypto.createHash("sha256").update(
+		`${bl.index};${bl.prevHash};${JSON.stringify(bl.data)};${bl.timestamp};${bl.nonce}`
+	).digest("hex");
 }
 
 function hashIsLowEnough(hash) {
-	// TODO
+	let binaryHash = hexToBinary(hash);
+
+	let leadingBits = binaryHash.substring(0,difficulty);
+
+	if (isZeroString(leadingBits)) return true;
+	else return false;
+}
+
+function hexToBinary(s) {
+	var ret = '';
+	for (var i = 0, len = s.length; i < len; i++) {
+		ret += hexToBinaryLookup[s[i]];
+	}
+	return ret;
+}
+
+function isZeroString(bitString) {
+	let zeroString = "";
+	for (let i = 0; i < bitString.length; i++) {
+		zeroString += "0";
+	}
+
+	if (bitString == zeroString) return true;
+	else return false;
 }
 
 function verifyBlock(bl) {
